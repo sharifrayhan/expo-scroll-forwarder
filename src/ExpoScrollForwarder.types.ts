5ee8555fa5a2612ec91935645fc821e336856256
Sharif Rayhan Nafi <sharifrayhan.official@gmail.com>
2025-10-26 14:23:57 +0600

    .

--- src/ExpoScrollForwarder.types.ts
+++
@@ -1,6 +0,0 @@
-import { type ReactNode } from "react";
-
-export interface ExpoScrollForwarderViewProps {
-  scrollViewTag: number | null;
-  children: ReactNode;
-}
--- src/ExpoScrollForwarderView.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { type ExpoScrollForwarderViewProps } from "./ExpoScrollForwarder.types";
-
-export function ExpoScrollForwarderView({
-  children,
-}: React.PropsWithChildren<ExpoScrollForwarderViewProps>) {
-  return children;
-}
